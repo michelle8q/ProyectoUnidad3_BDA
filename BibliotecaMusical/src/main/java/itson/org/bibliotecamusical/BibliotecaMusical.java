fcb115bb6ac1ca7bf3cec65d4ac810e1d294216e
michelle8q <cincastle584@gmail.com>
2026-07-05 19:00:21 -0700

    commit inicial

---
+++ BibliotecaMusical/src/main/java/itson/org/bibliotecamusical/BibliotecaMusical.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.org.bibliotecamusical;
+
+/**
+ *
+ * @author cinca
+ */
+public class BibliotecaMusical {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
