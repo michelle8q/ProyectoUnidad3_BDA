3a6f2b730911a74081f4d986616a376a5e897ff9
dannapina262718-tech <danna.pina262718@potros.itson.edu.mx>
2026-07-07 13:39:42 -0700

    paneles de menu y buscador

--- BibliotecaMusical/src/main/java/presentacion/PnlMenuLateral.java
+++ BibliotecaMusical/src/main/java/presentacion/PnlMenuLateral.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 package presentacion;
 
@@ -8,12 +8,12 @@ package presentacion;
  *
  * @author USUARIO
  */
-public class MenuLateral extends javax.swing.JFrame {
+public class PnlMenuLateral extends javax.swing.JPanel {
 
     /**
-     * Creates new form FrmMenuLateral
+     * Creates new form PnlMenuLateral
      */
-    public MenuLateral() {
+    public PnlMenuLateral() {
         initComponents();
     }
 
@@ -33,8 +33,11 @@ public class MenuLateral extends javax.swing.JFrame {
         LblCanciones = new javax.swing.JLabel();
         LblPerfil = new javax.swing.JLabel();
         BtnInsert = new javax.swing.JButton();
+        BtnCerrarSesion = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(235, 603));
+        setMinimumSize(new java.awt.Dimension(235, 603));
+        setPreferredSize(new java.awt.Dimension(235, 603));
 
         PnlFondo.setBackground(new java.awt.Color(51, 51, 51));
         PnlFondo.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(144, 104, 225)));
@@ -63,6 +66,15 @@ public class MenuLateral extends javax.swing.JFrame {
             }
         });
 
+        BtnCerrarSesion.setBackground(new java.awt.Color(0, 0, 0));
+        BtnCerrarSesion.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
+        BtnCerrarSesion.setText("Cerrar sesion");
+        BtnCerrarSesion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnCerrarSesionActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout PnlFondoLayout = new javax.swing.GroupLayout(PnlFondo);
         PnlFondo.setLayout(PnlFondoLayout);
         PnlFondoLayout.setHorizontalGroup(
@@ -70,18 +82,19 @@ public class MenuLateral extends javax.swing.JFrame {
             .addGroup(PnlFondoLayout.createSequentialGroup()
                 .addGap(30, 30, 30)
                 .addGroup(PnlFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BtnCerrarSesion, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(BtnInsert, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(LblPerfil)
                     .addComponent(LblArtistas)
                     .addComponent(LblAlbumes)
                     .addComponent(LblInicio)
                     .addComponent(LblCanciones))
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addContainerGap(48, Short.MAX_VALUE))
         );
         PnlFondoLayout.setVerticalGroup(
             PnlFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(PnlFondoLayout.createSequentialGroup()
-                .addGap(75, 75, 75)
+                .addGap(45, 45, 45)
                 .addComponent(LblInicio)
                 .addGap(51, 51, 51)
                 .addComponent(LblArtistas)
@@ -91,13 +104,15 @@ public class MenuLateral extends javax.swing.JFrame {
                 .addComponent(LblCanciones)
                 .addGap(51, 51, 51)
                 .addComponent(LblPerfil)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
+                .addComponent(BtnCerrarSesion, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(BtnInsert, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(41, 41, 41))
+                .addGap(21, 21, 21))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(PnlFondo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -108,51 +123,19 @@ public class MenuLateral extends javax.swing.JFrame {
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(PnlFondo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
-
-        pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void BtnInsertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnInsertActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_BtnInsertActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuLateral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuLateral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuLateral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuLateral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
+    private void BtnCerrarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCerrarSesionActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BtnCerrarSesionActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MenuLateral().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnCerrarSesion;
     private javax.swing.JButton BtnInsert;
     private javax.swing.JLabel LblAlbumes;
     private javax.swing.JLabel LblArtistas;
